5f9cf8c8c3de206204bdfdb84cbaa25f14339588
Jerry & Garry Incorporated <31044221+OwenKlutts@users.noreply.github.com>
2017-08-15 14:08:12 -0500

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,28 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		
+		int variableName = 5;
+		char character = 'c';
+		String hello = "Hello, world! ";
+		
+		
+		int x = 6;
+		int y = 10;
+		
+		
+		int n = 15;
+		int answer = 1;
+		
+		for(int i=1; i<=n; i=i+1)
+		{
+			answer = i*answer;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
